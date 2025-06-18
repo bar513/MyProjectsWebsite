b45e1aa665584f7d592ff4a166a6f7fc0061761b
bar <bar5132@gmail.com>
2025-06-18 19:10:46 +0300

    basic sketch

---
+++ server.py
@@ -0,0 +1,17 @@
+from http.server import HTTPServer, SimpleHTTPRequestHandler
+import webbrowser
+import os
+
+def run_server(port=8000):
+    server_address = ('', port)
+    httpd = HTTPServer(server_address, SimpleHTTPRequestHandler)
+    print(f"Server running at http://localhost:{port}")
+    
+    # Open the website in the default browser
+    webbrowser.open(f'http://localhost:{port}')
+    
+    # Start the server
+    httpd.serve_forever()
+
+if __name__ == '__main__':
+    run_server() 
